dcc85d0815592c085cead9fe2b408266c447b8d8
wafi04 <wafi3040@gmail.com>
2025-05-15 16:24:38 +0000

    fix:url k8s

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
-export const K8S_API_URL = "http://192.168.100.170:8001/api/v1";
-export const JUPYTER_URL = "https://test1.apps.zilog.zil/lab";
+export const K8S_API_URL =
+  process.env.KUBE_API_URL || process.env.NEXT_PUBLIC_KUBE_API_URL;
+export const JUPYTER_URL =
+  process.env.KUBE_JUPYTER_URL || process.env.NEXT_PUBLIC_KUBE_JUPYTER_URL;
 export const KUBE_TOKEN =
   process.env.KUBE_TOKEN || process.env.NEXT_PUBLIC_KUBE_TOKEN;
 export const LOGO = "/logo.png";
